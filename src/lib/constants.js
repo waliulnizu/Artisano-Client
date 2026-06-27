e70cd40bb70a3483d13b82f6d0d24da35a364bc8
waliulnizu <waliul.nizu@gmail.com>
2026-06-27 17:03:38 +0600

    Fix API URL for Vercel deployment

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,1 +1,5 @@
-export const API_URL = "http://localhost:5000/api";
+let apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api";
+if (apiUrl && !apiUrl.endsWith("/api")) {
+    apiUrl += "/api";
+}
+export const API_URL = apiUrl;
